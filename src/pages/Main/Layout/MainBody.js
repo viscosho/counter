9cc0a7e93faae986bf67623482a344162a748d04
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2021-01-06 10:25:29 -0300

    loop for getting the counters list

--- src/pages/Main/Layout/MainBody.js
+++ src/pages/Main/Layout/MainBody.js
@@ -4,6 +4,7 @@ import NoCounters from '../components/NoCounters';
 
 const MainBody = () => {
 	const [counters, setCounters] = useState(0);
+	const [counterList, setcounterList] = useState([]);
 
 	useEffect(() => {
 		let API_URL = `http://${window.location.hostname}:3001/api/v1/counter`;
@@ -11,13 +12,32 @@ const MainBody = () => {
 		fetch(API_URL)
 			.then((response) => response.json())
 			.then((json) => {
-				//console.log(json.length);
+				//console.log(json);
 				setCounters(json.length);
-				//console.log(counterList);
-			});
-	}, [counters]);
+				console.log(counterList);
+				json.forEach((counterItem) => {
+					let Item = {
+						count: counterItem.count,
+						id: counterItem.id,
+						title: counterItem.title
+					};
+					setcounterList((counterList) => [...counterList, Item]);
+				});
 
-	return <main>{counters ? <CounterList /> : <NoCounters />}</main>;
+				//console.log(counterList);
+				//console.log(setcounterList);
+			});
+		return;
+	}, []);
+
+	return (
+		<main>
+			{counterList.map((singleCounter) => (
+				<p key={singleCounter.title}>{singleCounter.title}</p>
+			))}
+			{counters ? <CounterList /> : <NoCounters />}
+		</main>
+	);
 };
 
 export default MainBody;
