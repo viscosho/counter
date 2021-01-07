451aaaeb04eedc3de861a4ff0ac55a5fa7711849
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2021-01-07 18:57:55 -0300

    Passing clicked times to list

--- src/pages/Main/Layout/MainBody.js
+++ src/pages/Main/Layout/MainBody.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { ListGroup } from 'react-bootstrap';
 import { GetCounterList } from '../../../components/Api';
 import CounterList from '../components/CounterList';
@@ -7,6 +7,9 @@ import NoCounters from '../components/NoCounters';
 const MainBody = () => {
 	const [counters, setCounters] = useState(0);
 	const [counterList, setCounterList] = useState([]);
+
+	const [count, setCount] = useState(0);
+	const increment = () => setCount(count + 1);
 
 	const getPosts = () => {
 		GetCounterList().then((json) => {
@@ -27,7 +30,25 @@ const MainBody = () => {
 	// 	console.log('state change');
 	// }, [counterList]);
 
-	return <section>{<ListGroup>{counters ? counterList.map((singleCounter) => <CounterList key={singleCounter.id} item={singleCounter} />) : <NoCounters />}</ListGroup>}</section>;
+	return (
+		<section>
+			<p>{counters} items</p>
+			<p>You clicked {count} times</p>
+			{
+				<ListGroup>
+					{counters ? (
+						counterList.map((singleCounter) => (
+							<Fragment>
+								<CounterList handleClick={increment} key={singleCounter.id} item={singleCounter} />
+							</Fragment>
+						))
+					) : (
+						<NoCounters />
+					)}
+				</ListGroup>
+			}
+		</section>
+	);
 };
 
 export default MainBody;
--- src/pages/Main/components/CounterList.js
+++ src/pages/Main/components/CounterList.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { ListGroup } from 'react-bootstrap';
 import { OperationCounter } from '../../../components/Api';
 
-const CounterList = (item) => {
+const CounterList = (item, count, handleClick) => {
 	const [counter, setCounter] = useState(item.item.count);
 	const [active, setActive] = useState(false);
 
@@ -15,15 +15,23 @@ const CounterList = (item) => {
 	const oneMore = () => {
 		OperationCounter(item.item.id, 'inc');
 		setCounter(counter + 1);
+		item.handleClick();
 	};
 
 	const oneLess = () => {
 		OperationCounter(item.item.id, 'dec');
 		setCounter(counter - 1);
+		item.handleClick();
 	};
 
 	return (
-		<ListGroup.Item key={item.item.id} onClick={deleteItem} className={`counter-item d-flex justify-content-between align-items-center mb-1 ${active ? 'item-selected' : null}`}>
+		<ListGroup.Item
+			key={item.item.id}
+			onClick={deleteItem}
+			className={`counter-item d-flex justify-content-between align-items-center mb-1 ${
+				active ? 'item-selected' : null
+			}`}
+		>
 			<p className="p-0 m-0 counter-name">{item.item.title}</p>
 			<div className="d-flex align-items-center counter-actions">
 				<button className="btn-calculation" onClick={oneLess}>
