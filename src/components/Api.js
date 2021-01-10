12f14cf1f8e7bc2e423c2be2539a12082e711293
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2021-01-10 17:22:33 -0300

    Cleaning

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,19 +1,5 @@
 const API_URL = `http://${window.location.hostname}:3001/api/v1/counter`;
 
-// export async function GetCounterList() {
-// 	return fetch(API_URL, {
-// 		headers: {
-// 			'Content-Type': 'application/json',
-// 			method: 'GET'
-// 		}
-// 	}).then((response) => {
-// 		if (!response.ok) {
-// 			throw new Error(response.statusText);
-// 		}
-// 		return response.json();
-// 	});
-// }
-
 export async function GetCounterList() {
 	const response = await fetch(API_URL, {
 		headers: {
