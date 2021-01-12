40dfe946bcf340da64ed39c0103b44477e7befeb
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2021-01-12 15:58:14 -0300

    Retry No connection button

--- src/components/NoConnection.js
+++ src/components/NoConnection.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import fetchCount from '../actions/fetchCountersActions';
+import { Button, Col, Row } from 'react-bootstrap';
 
 const NoConnection = () => {
+	const dispatch = useDispatch();
+	const onRetry = () => {
+		dispatch(fetchCount());
+	};
+
 	return (
-		<div className="d-flex flex-column justify-content-center align-content-center flex-wrap h-100">
+		<div className="no-connection p-3 d-flex flex-column justify-content-center align-content-center flex-wrap">
 			<h2 className="text-center">Couldn’t load the counters</h2>
 			<p className="text-center">The Internet connection appears to be offline.</p>
-			<Button variant="light">Retry</Button>
+			<Row>
+				<Col md={{ span: 4, offset: 4 }} className="d-flex flex-column justify-content-center align-content-center">
+					<Button variant="light" onClick={() => onRetry()}>
+						Retry
+					</Button>
+				</Col>
+			</Row>
 		</div>
 	);
 };
