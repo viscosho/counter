c2cc0b1d391cc56d7bd3b23726dbcc4117d37720
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2021-01-12 12:27:51 -0300

    Testing adding build folder

---
+++ build/precache-manifest.52aebadeb3128f3ab0a030354c76df16.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5bfbd334361b868fdefddde6d8221d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f5ab09e931afa0fa26f",
+    "url": "/static/css/main.c4c25dfe.chunk.css"
+  },
+  {
+    "revision": "c1a67caa251a003e7ad9",
+    "url": "/static/js/2.6e0d5a4c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.6e0d5a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5ab09e931afa0fa26f",
+    "url": "/static/js/main.717a8903.chunk.js"
+  },
+  {
+    "revision": "44269ce1ffba5adbeab2",
+    "url": "/static/js/runtime-main.7892ef48.js"
+  },
+  {
+    "revision": "6fa90141aee6feca53fc6a43e3b2ac21",
+    "url": "/static/media/icon.6fa90141.svg"
+  },
+  {
+    "revision": "5e7dba9ec67959c55c58e10ca111af26",
+    "url": "/static/media/nunito-black-webfont.5e7dba9e.woff"
+  },
+  {
+    "revision": "e6925da7340afc8a32ea2abc71941d70",
+    "url": "/static/media/nunito-black-webfont.e6925da7.woff2"
+  },
+  {
+    "revision": "b80985a07db86a57e0e080445bff17fa",
+    "url": "/static/media/nunito-blackitalic-webfont.b80985a0.woff"
+  },
+  {
+    "revision": "f8616cdf623ebd44bd46057602e49ad3",
+    "url": "/static/media/nunito-blackitalic-webfont.f8616cdf.woff2"
+  },
+  {
+    "revision": "0d34151055831a9099c185f759a7d43e",
+    "url": "/static/media/nunito-bold-webfont.0d341510.woff"
+  },
+  {
+    "revision": "37045f3bea421a9bf753fe45f4352d5c",
+    "url": "/static/media/nunito-bold-webfont.37045f3b.woff2"
+  },
+  {
+    "revision": "18b227c1f5ab3f60187aae2bb1b457be",
+    "url": "/static/media/nunito-bolditalic-webfont.18b227c1.woff"
+  },
+  {
+    "revision": "ed0492ae1429393c334e78451a50df54",
+    "url": "/static/media/nunito-bolditalic-webfont.ed0492ae.woff2"
+  },
+  {
+    "revision": "168233c5d11429d8f7d0065b09dc1b25",
+    "url": "/static/media/nunito-extrabold-webfont.168233c5.woff2"
+  },
+  {
+    "revision": "b3a2eba57905e3140040d7a16d2119ff",
+    "url": "/static/media/nunito-extrabold-webfont.b3a2eba5.woff"
+  },
+  {
+    "revision": "475377484e0e4225267f1061d05c655f",
+    "url": "/static/media/nunito-extrabolditalic-webfont.47537748.woff2"
+  },
+  {
+    "revision": "87e52cbe6f7d87e1d1c1eacce0a5dd6f",
+    "url": "/static/media/nunito-extrabolditalic-webfont.87e52cbe.woff"
+  },
+  {
+    "revision": "76520ac439d748f7538f8aae20b5f000",
+    "url": "/static/media/nunito-extralight-webfont.76520ac4.woff2"
+  },
+  {
+    "revision": "f8503b2c3bdf962d2c475c3f7def1c51",
+    "url": "/static/media/nunito-extralight-webfont.f8503b2c.woff"
+  },
+  {
+    "revision": "11f421f406767e37ac9b32007d236fdf",
+    "url": "/static/media/nunito-extralightitalic-webfont.11f421f4.woff"
+  },
+  {
+    "revision": "c9e9e4fee5b52b1d0fa704147e5ff0d7",
+    "url": "/static/media/nunito-extralightitalic-webfont.c9e9e4fe.woff2"
+  },
+  {
+    "revision": "004e77503c3c959928d394fa0a96c193",
+    "url": "/static/media/nunito-italic-webfont.004e7750.woff"
+  },
+  {
+    "revision": "17299839f0a0e5c66ebc511f3daa5d92",
+    "url": "/static/media/nunito-italic-webfont.17299839.woff2"
+  },
+  {
+    "revision": "161bd970eee1d2b455d05b6cff82904c",
+    "url": "/static/media/nunito-light-webfont.161bd970.woff2"
+  },
+  {
+    "revision": "50e5c3727aecb1a849d78e2b9959c6ed",
+    "url": "/static/media/nunito-light-webfont.50e5c372.woff"
+  },
+  {
+    "revision": "9cd155c86a6bdc3216c97d7ea7035225",
+    "url": "/static/media/nunito-lightitalic-webfont.9cd155c8.woff2"
+  },
+  {
+    "revision": "a0de57e46f559c0eecc1db5ac166ef8f",
+    "url": "/static/media/nunito-lightitalic-webfont.a0de57e4.woff"
+  },
+  {
+    "revision": "831e6efd8c91a974d8782e70bfa1b0d8",
+    "url": "/static/media/nunito-regular-webfont.831e6efd.woff"
+  },
+  {
+    "revision": "cf44e1751a4c159e9ebf6cd0eb27dbc7",
+    "url": "/static/media/nunito-regular-webfont.cf44e175.woff2"
+  },
+  {
+    "revision": "7a233df331076bb643445002053173bb",
+    "url": "/static/media/nunito-semibold-webfont.7a233df3.woff2"
+  },
+  {
+    "revision": "ef30bcebe8ea5a2036f9d395b33039e2",
+    "url": "/static/media/nunito-semibold-webfont.ef30bceb.woff"
+  },
+  {
+    "revision": "3d339ce4d1ea44f2a2cb054f7de46866",
+    "url": "/static/media/nunito-semibolditalic-webfont.3d339ce4.woff"
+  },
+  {
+    "revision": "96bdebcb1096d82c014e88575fda2019",
+    "url": "/static/media/nunito-semibolditalic-webfont.96bdebcb.woff2"
+  },
+  {
+    "revision": "4ccdd3f02bc24fe02db1b2eca9eef037",
+    "url": "/static/media/paper_note.4ccdd3f0.svg"
+  }
+]);
